30174215ea395b392e99a113107c63e6679575ed
Corentin-Vidick <corentin.vidick@gmail.com>
2022-11-06 16:11:36 +0000

    Add map creation function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
     playGame();
 })
 
+/**
+ * Creates contents of game-area
+ */
+ function createMap() {
+    document.getElementById("game-container").innerHTML = "";
+    for (let x = 0; x < 100; x++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
+        cell.id = x;
+        document.getElementById("game-container").appendChild(cell);
+    }
+}
 
 /**
  * Menu page
